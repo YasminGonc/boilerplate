db46c8c3c2e6aef14fd241dfea97c17e5ad0b478
Yasmin Gonçalves <yasmin.goncalves97@hotmail.com>
2023-01-31 14:56:20 -0300

    Fix: jest files and storybook script

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('jest').Config} */
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const nextJest = require('next/jest')
 
 const createJestConfig = nextJest({
@@ -11,7 +12,7 @@ const customJestConfig = {
   testPathIgnorePatterns: ['/node_modules', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)']
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*.stories.tsx']
 }
 
 module.exports = createJestConfig(customJestConfig)
